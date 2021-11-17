84f6d84719f05142cb179f8d8b096700b225b672
arakotomahefa <alison.rakotomahefa@arolla.fr>
2021-11-17 17:20:41 +0100

    divisible by 5

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,3 +1,9 @@
 def return_fizz(number):
     if number % 3 == 0:
         return ("Fizz")
+
+def return_buzz(number):
+    if number % 5 == 0:
+        return ("Buzz")
+
+
--- fizz_buzz_test.py
+++ fizz_buzz_test.py
@@ -1,12 +1,18 @@
-from fizz_buzz import return_fizz
+from fizz_buzz import return_fizz,return_buzz
 
 # "Fizz" if %3
 # "buzz" if %5
-# "FizzBuzz" if %3 %5
+# "FizzBuzz" if %3 && %5
 # sinon le meme nombre si no match
 
 def test_should_return_fizz():
     divisible_by_3 = 15
     actual = return_fizz(divisible_by_3)
     expected = "Fizz"
+    assert actual == expected
+
+def test_should_return_buzz():
+    divisible_by_5 = 20
+    actual = return_buzz(divisible_by_5)
+    expected = "Buzz"
     assert actual == expected
